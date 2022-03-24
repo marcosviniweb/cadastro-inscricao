a1a7124f8f3a8f6d3ea3256befc75140df8874d4
Marcos Ribeiro <63163857+marcosviniweb@users.noreply.github.com>
2022-03-24 14:36:20 -0300

    Novo campo Sus / nomeclatura (opcional)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,7 +18,8 @@ export class HomeComponent implements OnInit {
        n:'',
        endereco : '',
        complemento: '',
-       obs: ''
+       obs: '',
+       sus: ''
     }
   constructor() { }
 
